736e45171e52482fa4b643cda76b4347bc4b3c2c
amil-ck <amil.ck@icloud.com>
2025-05-27 02:48:40 +0100

    various diceroll problems fixed

--- client/src/Components/lib/diceRoll.js
+++ client/src/Components/lib/diceRoll.js
@@ -1,11 +1,31 @@
-export default function diceRoll(diceSides, modifier=0, advantage=false) {
-    let roll1 = Math.ceil(Math.random() * diceSides);
-    if (advantage) {
-        let roll2 = Math.ceil(Math.random() * diceSides);
-        if (roll2 > roll1) {
-            roll1 = roll2;
-        }
+export default function diceRoll(diceSides=20, noDice=1, modifier=0, advantage="none") {
+    let rolls = []
+
+    for (let i = 0; i < noDice; i++) {
+        rolls.push(Math.ceil(Math.random() * diceSides));
     }
 
-    return roll1 + modifier;
+    rolls.push(Math.ceil(Math.random() * diceSides));
+    //sorted rolls
+    let s_rolls = [...rolls];
+
+    if (advantage === "advantage") {
+        s_rolls.sort((a, b) => (a - b));
+        s_rolls.pop();
+    } else if (advantage === "disadvantage") {
+        s_rolls.sort((a, b) => (b - a));
+        s_rolls.pop();
+    } else {
+        //removing the extra dice only used for (dis)advantaged rolls from history
+        rolls.pop();
+        s_rolls.pop();
+    }
+
+    let total = 0;
+    s_rolls.forEach(e => total += e);
+
+    return {
+        "value": total,
+        "rolls": rolls
+    };
 }
