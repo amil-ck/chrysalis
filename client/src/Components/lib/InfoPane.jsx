0a8c3c7dfa69fa9497ccc9e1d3823056b582d010
Charlie Appleton <xjarlie@gmail.com>
2025-05-28 15:14:30 +0100

    add basic InfoPane functionality

--- client/src/Components/lib/InfoPane.jsx
+++ client/src/Components/lib/InfoPane.jsx
@@ -4,14 +4,35 @@ export default class InfoPane extends React.Component {
     constructor(props) {
         super();
         this.props = props;
-        // Props: itemdata, itemtype
+        // Props: data: {title: string, subtitle: string, attributes: {string: string}, description: god knows, source: string }
 
     }
 
+    parseDescription(descObj) {}
+
     render() {
+
+        if (this.props.data.title === undefined) {
+            return <div className='infoPane'></div>
+        }
+
         return (
             <div className='infoPane'>
-
+                <div className='header'>
+                    <span className='title'>{this.props.data.title}</span>
+                    <span className='subtitle'>{this.props.data.subtitle}</span>
+                </div>
+                <div className='attributes'>
+                    {Object.entries(this.props.data.attributes).map(([key, value]) => {
+                        return <span className='attribute' key={key}>{key}: {value}</span>
+                    })}
+                </div>
+                <div className='description'>
+                    {JSON.stringify(this.props.data.description)}
+                </div>
+                <div className='footer'>
+                    Source: {this.props.data.source}
+                </div>
             </div>
         )
     }
