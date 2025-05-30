4b4d40f884309dca6f390fa3d4ded08d4fb7fc9a
Charlie Appleton <xjarlie@gmail.com>
2025-05-30 17:17:30 +0100

    spell, feat and generic info panes complete

--- client/src/Components/lib/GenericInfoPane.jsx
+++ client/src/Components/lib/GenericInfoPane.jsx
@@ -1,37 +1,39 @@
 import * as React from 'react';
 
-export default class InfoPane extends React.Component {
+export default class GenericInfoPane extends React.Component {
     constructor(props) {
         super();
         this.props = props;
         // Props: data: {title: string, subtitle: string, attributes: {string: string}, description: god knows, source: string }
+        // onClose: function()
 
     }
 
-    parseDescription(descObj) {}
-
     render() {
 
-        if (this.props.data.title === undefined) {
-            return <div className='infoPane'></div>
+        if (this.props.data?.title === undefined) {
+            return <div className='infoPane hidden'></div>
         }
 
         return (
             <div className='infoPane'>
                 <div className='header'>
-                    <span className='title'>{this.props.data.title}</span>
-                    <span className='subtitle'>{this.props.data.subtitle}</span>
+                    <div className="titleWrapper">
+                        <span className='title'>{this.props.data.title}</span>
+                        <span className='subtitle'>{this.props.data.subtitle}</span>
+                    </div>
+                    <button className='closeButton' onClick={() => {if (this.props.onClose) this.props.onClose()}}>x</button>
                 </div>
                 <div className='attributes'>
                     {Object.entries(this.props.data.attributes).map(([key, value]) => {
-                        return <span className='attribute' key={key}>{key}: {value}</span>
+                        return <span className='attribute' key={key}><b>{key}:</b> {value}</span>
                     })}
                 </div>
                 <div className='description'>
                     {JSON.stringify(this.props.data.description)}
                 </div>
                 <div className='footer'>
-                    Source: {this.props.data.source}
+                    <b>Source:</b> {this.props.data.source}
                 </div>
             </div>
         )
