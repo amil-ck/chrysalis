1401e3ab473cd2c4da5db38ead9710ef1cbdcae7
amil-ck <amil.ck@icloud.com>
2025-05-31 19:41:12 +0100

    Adding and removing from class page now works fully

--- client/src/Components/creation/creationTabs/ClassSelection.jsx
+++ client/src/Components/creation/creationTabs/ClassSelection.jsx
@@ -15,7 +15,8 @@ export default class ClassSelection extends React.Component {
             grants: [],
             listsNeeded: [],
             listsData: {},
-            choices: []
+            choices: [],
+            choiceDict: {}
         }
 
         this.onFeatureDoubleSelected = this.onFeatureDoubleSelected.bind(this);
@@ -31,58 +32,107 @@ export default class ClassSelection extends React.Component {
         return array.filter(e => e[type] === value)
     }
 
+    getFromId(id) {
+        return CLASSES.find(e => e.id === id)
+    }
+
+    checkChoices(choices) {
+        if (choices.length > 1) {
+            console.log(choices);
+            console.log(this.getChoices(choices[0]));
+        }
+
+        const newChoices = choices.filter(x => {
+            const slice = choices.slice(0, choices.indexOf(x));
+            return (slice.some(y => {
+                const xElement = this.getFromId(x); 
+                if (xElement?.supports !== undefined) {
+                    return this.getChoices(y).includes(xElement.supports[0]);
+                };
+                return false;
+            }) || this.getFromId(x).type === "Class");
+        })
+
+        const filteredChoice = choices.filter(x => !newChoices.includes(x));
+
+        return {
+            "choices": newChoices,
+            "invalidList": filteredChoice
+        }
+    }
+
+
     access = (path, object) => {
         return path.split('.').reduce((o, i) => o[i], object)
     }
 
     updateStuff() {
+        let choiceData = this.checkChoices([...this.state.choices]);
+        let choices = choiceData.choices;
+
+        for (const invalid of choiceData.invalidList) {
+            const choiceArray = this.access(this.getFromId(invalid).supports[0], this.state.listsData);
+            choiceArray.splice(0, choiceArray.length);
+        }
+
+
+        // let choices = [...this.state.choices];
         let newList = [];
-        for (const id of this.state.choices) {
-            const idList = [];
-            const grant = CLASSES.find(e => e.id === id).rules?.grant
-            if (grant !== undefined) {
-                grant.forEach(
-                    e => {
-                        if (e.level === undefined || parseInt(e.level) <= this.state.level) {
-                            if (e.number === undefined) {
+        console.log(choices);
+        for (const id of choices) {
+            newList.push(...this.getChoices(id));
+        }
+        this.setState({listsNeeded: [...newList]});
+        this.setState({choices: [...choices]});
+    }
+
+    getChoices(id) {
+        let newList = [];
+        const idList = [];
+        const grant = CLASSES.find(e => e.id === id).rules?.grant
+        if (grant !== undefined) {
+            grant.forEach(
+                e => {
+                    if (e.level === undefined || parseInt(e.level) <= this.state.level) {
+                        if (e.number === undefined) {
+                            idList.push(e.id)
+                        } else {
+                            for (let i = 0; i < parseInt(e.number); i++) {
                                 idList.push(e.id)
+                            }
+                        }
+                    }
+                }
+            )
+        }
+
+        idList.push(id);
+        console.log(idList);
+
+        for (const eId of idList) {
+            const select = CLASSES.find(e => e.id === eId)?.rules?.select
+            if (select !== undefined) {
+                select.forEach(
+                    e => {
+                        //the e.supports !== undefined is for ranger's favoured enemy which gives you language (deal with this properly later)
+                        if (e.supports !== undefined && (e.level === undefined || parseInt(e.level) <= this.state.level)) {
+                            if (e.number === undefined) {
+                                console.log(e);
+                                newList.push(e.supports[0])
                             } else {
                                 for (let i = 0; i < parseInt(e.number); i++) {
-                                    idList.push(e.id)
+                                    newList.push(e.supports[0])
                                 }
                             }
                         }
                     }
                 )
             }
-
-            idList.push(id);
-            console.log(idList);
-
-            for (const eId of idList) {
-                const select = CLASSES.find(e => e.id === eId)?.rules?.select
-                if (select !== undefined) {
-                    select.forEach(
-                        e => {
-                            //the e.supports !== undefined is for ranger's favoured enemy which gives you language (deal with this properly later)
-                            if (e.supports !== undefined && (e.level === undefined || parseInt(e.level) <= this.state.level)) {
-                                if (e.number === undefined) {
-                                    console.log(e);
-                                    newList.push(e.supports[0])
-                                } else {
-                                    for (let i = 0; i < parseInt(e.number); i++) {
-                                        newList.push(e.supports[0])
-                                    }
-                                }
-                            }
-                        }
-                    )
-                }
-            }
         }
-        this.setState({listsNeeded: [...newList]});
-    }
 
+        return newList;
+
+    }
 
     
     onFeatureSelected(id) {
@@ -91,7 +141,6 @@ export default class ClassSelection extends React.Component {
 
     onFeatureDoubleSelected(id, array) {
         let actualArray = this.access(array, this.state)
-        console.log(actualArray)
         // if (this.state[array] === undefined) {
         //     this.setState({
         //         [array]: []
@@ -115,11 +164,14 @@ export default class ClassSelection extends React.Component {
             console.log('feat removed: ' + id)
 
             actualArray.splice(actualArray.findIndex(e => e === id), 1);
-
-            this.state.choices.splice(this.state.choices.findIndex(e => e === id), 1);
-            this.setState({
-                choices: this.state.choices
-            })
+            
+            //technically this won't be a necessary check when i fix some other stuff
+            if (this.state.choices.includes(id)) {
+                this.state.choices.splice(this.state.choices.findIndex(e => e === id), 1);
+                this.setState({
+                    choices: this.state.choices
+                })
+            }
 
             this.updateStuff();
 
