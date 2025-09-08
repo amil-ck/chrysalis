a76ac001c73a0a75fbbd97501c4fa5e49b80e196
Charlie Appleton <xjarlie@gmail.com>
2025-09-08 02:52:09 +0100

    add magic tab to play

--- client/src/Components/game/Game.jsx
+++ client/src/Components/game/Game.jsx
@@ -1,61 +1,24 @@
 import * as React from 'react';
+import Battle from './Battle.jsx';
+import Magic from './Magic.jsx';
 
 export default class Game extends React.Component {
     constructor(props) {
         super();
         this.props = props;
 
-        this.state = {}
+        this.state = {
+            tab: 'magic'
+        }
     }
 
     render() {
-        if (this.props.characterData.id === undefined) {
-            return (<>No character selected</>)
+        if (this.state.tab === 'battle') {
+            return <Battle {...this.props} />
+        } else if (this.state.tab === 'magic') {
+            return <Magic {...this.props} />
+        } else {
+            return <>beep boop</>
         }
-
-        const stats = [{name: "STR", value: 16, modifier: 3}, {name: "DEX", value: 9, modifier: -1}, {name: "CON", value: 15, modifier: 2}, {name: "INT", value: 11, modifier: 0}, {name: "WIS", value: 13, modifier: +1}, {name: "CHA", value: 14, modifier: +2}];
-
-        return (
-            <div className="play fullPane">
-                <div className="tab battle">
-                    <div className="header">
-                        <div className="details card">
-                            <div className="body">
-                                <span className="name">{this.props.characterData.name}</span>
-                                <span className="details">Level {this.props.characterData.level || "unknown"} {this.props.characterData.class || "Class unknown"}</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="main">
-                        <div className="stats col card">
-                            <span className="title">Stats</span>
-                            <div className="body">
-                                {stats.map(stat => {
-                                    return (
-                                        <div className="stat">
-                                            <span className="name">{stat.name}</span>
-                                            <span className="modifier">{stat.modifier >= 0 ? `+${stat.modifier}` : stat.modifier}</span>
-                                            <span className="value">{stat.value}</span>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                        <div className="skills col">
-                            <div className="savingThrows card">
-                                <span className="title">Saving throws</span>
-                            </div>
-                            <div className="skillsList card">
-                                <span className="title">Skills</span>
-                            </div>
-                        </div>
-                        <div className="misc col">
-                            <div className="topRow"></div>
-                            <div className="actions card"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
     }
 }
---
+++ client/src/Components/game/Magic.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export default class Magic extends React.Component {
+    constructor(props) {
+        super();
+
+        this.props = props;
+
+        // Assume that the character CAN spellcast
+        
+    }
+
+    render() {
+
+    }
+}
